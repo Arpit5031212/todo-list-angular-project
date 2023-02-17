160d71feda19f6e95a08faf98b81c4c72f1ccdf0
Arpit5031212 <shuklarpit2015@gmail.com>
2023-02-17 11:24:58 +0530

    todo app version 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'to-do-list-app';
+  taskList : any[] = [];
+  @ViewChild('task') taskName : any;
+
+  addTask(task : string) {
+    this.taskList.push({id : this.taskList.length, name : task});
+    this.taskName.nativeElement.value = "";
+  }
+
+  removeTask(id : number) {
+    this.taskList = this.taskList.filter(task => task.id !== id);
+  }
 }
